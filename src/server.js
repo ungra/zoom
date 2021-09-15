1fcdd2306b51050db8b48754f76acaa015787095
Jun Hee Lee <junheelee@Junui-iMac.local>
2021-09-15 16:49:45 +0900

    #0.4 Recap

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, resp) => resp.render("home"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 
